bd7044c82711e7fbe34075ba1a110bd37267b69d
Joseph <nuclearnarwhal2016@gmail.com>
2023-05-21 19:56:20 -0400

    virtual environment + some changes to the visualization

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(42)
 df = pd.read_csv('AmesHousing.csv')
 
-price_samp = df.sample(n=500, replace = False)
+price_samp = df.sample(n=500, replace=False)
 
 num_iterations = 1000000
 
@@ -15,17 +15,20 @@ for _ in range(num_iterations):
     df_medians.append(np.median(resample))
 
 fig, axs = plt.subplots(1, 3, figsize=(10, 8))
-axs[0].hist(df_medians, bins=30, density=True, color='skyblue', edgecolor = 'black')
+axs[0].hist(df_medians, bins=30, density=True,
+            color='skyblue', edgecolor='black')
 axs[0].set_title('Median histogram')
 axs[0].set_xlabel("Prices")
 axs[0].set_ylabel("Proportion")
 
-axs[1].hist(df['SalePrice'], bins=30, density=True, color ='skyblue', edgecolor = 'black')
+axs[1].hist(df['SalePrice'], bins=30, density=True,
+            color='skyblue', edgecolor='black')
 axs[1].set_title('Population histogram')
 axs[1].set_xlabel("Prices")
 axs[1].set_ylabel("Proportion")
 
-axs[2].hist(price_samp['SalePrice'], bins=10, density=True, color ='skyblue', edgecolor = 'black')
+axs[2].hist(price_samp['SalePrice'], bins=10, density=True,
+            color='skyblue', edgecolor='black')
 axs[2].set_title('Sample histogram')
 axs[2].set_xlabel("Prices")
 axs[2].set_ylabel("Proportion")
